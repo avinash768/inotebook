7c1991c052ab220f7eaaf6ae03dc709f20cb0960
avinash waghmare <avinashwaghmare321@gmail.com>
2023-08-27 23:57:34 +0530

    connection complit

---
+++ backend/models/User.js
@@ -0,0 +1,24 @@
+const mongoose = require('mongoose')
+const {Schema} = mongoose
+
+const UserSchema = new Schema({
+    name:{
+        type: String,
+        requrired: true
+    },
+    password:{
+        type: String,
+        requrired: true
+    },
+    email:{
+        type:String,
+        requrired: true,
+        unique:true
+    },
+    date:{
+        type: Date,
+        default: Date.now
+    },
+})
+
+module.exports = mongoose.model('user',UserSchema);
