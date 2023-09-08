734cd70a540c71bb7e2e6b9ff08fc3c59ecbb56e
avinash waghmare <avinashwaghmare321@gmail.com>
2023-09-08 12:48:37 +0530

    login done

--- backend/routes/auth.js
+++ backend/routes/auth.js
@@ -8,7 +8,7 @@ const jwt = require('jsonwebtoken');
 const JWT_SECRET = 'avinsha$me';
 
 
-//used post "/api/auth/createuser"
+//used post "/api/auth/createuser" not login required
 router.post('/createuser', [
     body('name', 'Enter a valid name').isLength({ min: 3 }),
     body('password', 'Enter a valid password').isLength({ min: 5 }),
@@ -54,8 +54,52 @@ router.post('/createuser', [
         //catch errors
     } catch (error) {
         console.log(error.massage);
-        res.status(500).send("same error occured");
+        res.status(500).send("Internal server error");
     }
+})
+
+
+
+//used post "/api/auth/login"login required
+router.post('/login', [
+    body('email', 'Enter a valid email').isEmail(),
+    body('password', 'Enter a valid password').exists(),    
+], async (req, res) => {
+    
+
+    // any error accuared send bad request
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+        return res.status(400).json({ errors: errors.array() });
+    }
+    const {email, password}=req.body;
+    try {
+        
+        let user = await User.findOne({email});
+        if(!user){
+            return res.status(400).json({error: "please try to login with correct credentions"});
+        }
+
+        const passwordCompare = await bcrypt.compare(password, user.password);
+        if(!passwordCompare){
+            return res.status(400).json({error: "please try to login with correct credentions"});
+        }
+
+        const data = {
+            user:{
+                id: user.id
+            }
+        }
+        //data convert into authtoken
+        const authtoken = jwt.sign(data, JWT_SECRET);
+        // console.log(jwtData);
+        res.json({authtoken});
+
+    } catch (error) {
+        console.log(error.massage);
+        res.status(500).send("Internal server error");
+    }
+
 })
 
 module.exports = router
